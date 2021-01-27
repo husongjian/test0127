976786f29d1e0355f192900364ff65331b8bd267
husj <husj@eastcom-sw.com>
2021-01-27 11:01:34 +0800

    假设我修改了master下的东西

--- src/main/java/com/TestMain.java
+++ src/main/java/com/TestMain.java
@@ -2,9 +2,10 @@ package com;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("0127的git尝试");
-        System.out.println("0127的git尝试");
-        System.out.println("0127的git尝试");
-        System.out.println("0127的git尝试");
+        System.out.println("假设这是我修改了master分支下的东西");
+        System.out.println("假设这是我修改了master分支下的东西");
+        System.out.println("假设这是我修改了master分支下的东西");
+        System.out.println("假设这是我修改了master分支下的东西");
+        System.out.println("假设这是我修改了master分支下的东西");
     }
 }
