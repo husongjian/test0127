4d362b8a60c6c317d67e7faf3e1857d78eea87af
husj <husj@eastcom-sw.com>
2021-01-27 11:08:34 +0800

    master又又又修改了master分支

--- src/main/java/com/TestMain.java
+++ src/main/java/com/TestMain.java
@@ -2,14 +2,12 @@ package com;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("我又修改了master分支");
-        System.out.println("我又修改了master分支");
-        System.out.println("我又修改了master分支");
-        System.out.println("我又修改了master分支");
-        System.out.println("我又修改了master分支");
-        System.out.println("我又修改了master分支");
-        System.out.println("我又修改了master分支");
-        System.out.println("我又修改了master分支");
-        System.out.println("我又修改了master分支");
+        System.out.println("master又又又修改了master分支");
+        System.out.println("master又又又修改了master分支");
+        System.out.println("master又又又修改了master分支");
+        System.out.println("master又又又修改了master分支");
+        System.out.println("master又又又修改了master分支");
+        System.out.println("master又又又修改了master分支");
+        System.out.println("master又又又修改了master分支");
     }
 }
