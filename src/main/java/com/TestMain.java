1d3e1154638aecbbb2a7612a01c8a645fb65b60c
husj <husj@eastcom-sw.com>
2021-01-27 11:16:20 +0800

    mastet在工作

--- src/main/java/com/TestMain.java
+++ src/main/java/com/TestMain.java
@@ -2,13 +2,12 @@ package com;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("我又在疯狂的工作");
-        System.out.println("我又在疯狂的工作");
-        System.out.println("我又在疯狂的工作");
-        System.out.println("我又在疯狂的工作");
-        System.out.println("我又在疯狂的工作");
-        System.out.println("我又在疯狂的工作");
-        System.out.println("我又在疯狂的工作");
-        System.out.println("我又在疯狂的工作");
+        System.out.println("mastet在工作");
+        System.out.println("mastet在工作");
+        System.out.println("mastet在工作");
+        System.out.println("mastet在工作");
+        System.out.println("mastet在工作");
+        System.out.println("mastet在工作");
+        System.out.println("mastet在工作");
     }
 }
