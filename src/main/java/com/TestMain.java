296b6e48adadac6cfaa129ca8f0d4022b6088dab
husj <husj@eastcom-sw.com>
2021-01-27 10:46:41 +0800

    aaaaaaa

---
+++ src/main/java/com/TestMain.java
@@ -0,0 +1,10 @@
+package com;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("0127的git尝试");
+        System.out.println("0127的git尝试");
+        System.out.println("0127的git尝试");
+        System.out.println("0127的git尝试");
+    }
+}
