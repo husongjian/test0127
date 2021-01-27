7076f500ccc982b5472f7f6b61ed56948b880d85
husj <husj@eastcom-sw.com>
2021-01-27 11:04:01 +0800

    我又修改了master分支

--- src/main/java/com/TestMain.java
+++ src/main/java/com/TestMain.java
@@ -2,10 +2,14 @@ package com;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("假设这是我修改了master分支下的东西");
-        System.out.println("假设这是我修改了master分支下的东西");
-        System.out.println("假设这是我修改了master分支下的东西");
-        System.out.println("假设这是我修改了master分支下的东西");
-        System.out.println("假设这是我修改了master分支下的东西");
+        System.out.println("我又修改了master分支");
+        System.out.println("我又修改了master分支");
+        System.out.println("我又修改了master分支");
+        System.out.println("我又修改了master分支");
+        System.out.println("我又修改了master分支");
+        System.out.println("我又修改了master分支");
+        System.out.println("我又修改了master分支");
+        System.out.println("我又修改了master分支");
+        System.out.println("我又修改了master分支");
     }
 }
